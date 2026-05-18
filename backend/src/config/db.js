1757755a78f5fd9cb51a5dbf5ed3557ba516746a
Deepak varshney <manojdeepak145@gmail.com>
2026-05-18 14:20:19 +0530

    Fixed MongoDB TLS issue

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,8 +1,23 @@
 import mongoose from 'mongoose';
 
 export async function connectDB(uri) {
-  if (!uri) throw new Error('MONGO_URI is required');
-  mongoose.set('strictQuery', true);
-  await mongoose.connect(uri);
-  console.log('MongoDB connected');
+  try {
+    if (!uri) {
+      throw new Error('MONGO_URI is required');
+    }
+
+    mongoose.set('strictQuery', true);
+
+    await mongoose.connect(uri, {
+      tls: true,
+      retryWrites: true,
+      w: 'majority',
+    });
+
+    console.log('MongoDB connected');
+  } catch (error) {
+    console.error('MongoDB connection error:', error);
+
+    process.exit(1);
+  }
 }
